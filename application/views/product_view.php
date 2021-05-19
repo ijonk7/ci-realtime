79a7d4bd0633c2ec41f675d6c56cf79316436bbe
ijonk7 <jookeo.rizal@gmail.com>
2021-05-19 15:03:56 +0700

    update price column

--- application/views/product_view.php
+++ application/views/product_view.php
@@ -166,6 +166,21 @@
                 }
             });
 
+            
+            function convert_rupiah(bilangan) {
+                var	number_string = bilangan.toString(),
+                    sisa 	= number_string.length % 3,
+                    rupiah 	= number_string.substr(0, sisa),
+                    ribuan 	= number_string.substr(sisa).match(/\d{3}/g);
+                        
+                if (ribuan) {
+                    separator = sisa ? '.' : '';
+                    rupiah += separator + ribuan.join('.');
+                }
+
+                return rupiah;
+            }
+
             // FUNCTION SHOW PRODUCT
             function show_product() {
                 $.ajax({
@@ -185,7 +200,7 @@
                                 '<td>' +
                                 '<a href="javascript:void(0);" class="btn btn-sm btn-warning item_edit" data-id="' +
                                 data[i].product_id + '" data-name="' + data[i].product_name +
-                                '" data-price="' + number_format(data[i].product_price,0,",",".") + '">Edit</a>&nbsp;' +
+                                '" data-price="' + convert_rupiah(data[i].product_price) + '">Edit</a>&nbsp;' +
                                 '<a href="javascript:void(0);" class="btn btn-sm btn-danger item_delete" data-id="' +
                                 data[i].product_id + '">Delete</a>' +
                                 '</td>' +
